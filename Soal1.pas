45594bc07c4446950de171f0bc6740187724f692
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2024-10-24 11:41:36 +0700

    Soal1.pas
    JAWABAN SOAL 1 UTS DP 1

--- Soal1.pas
+++ Soal1.pas
@@ -1,59 +1,28 @@
-//ketik kode jawaban no 1 kamu disiniprogram uts1; //nama program (opsional)
-uses crt; //untuk clearscreen hihi :3
+program harga_jarak; //nama program (opsional)
+uses crt; // untuk clearscreen
 var 
-    km,total,total1:real; //variabel 
+    km,total,total1:real; //variabel
     prem:string; //variabel
 begin //mulai
-    clrscr; //membersihkan hal yang gak penting (clearscreen)
-    Write('Jarak (KM) : '); readln(km); //Input jarak
-    Write('Anggota Premium : '); readln(prem); //Input anggota premium atau nggak
-
-    if (km > 10) then //apakah km lebih dari 10?
-    begin //mulai jika iya
-     total1:= km*3000 + 20000; //menghitung dasar harga tanpa diskon
-        if (prem = 'True') or (prem = 'true') then //apakah anggota premium
-        begin //mulai jika anggota premium
-        total:= total1 * 0.95; //kalau anggota premium dapat diskon 5%
-            if (total1 > 100000) then //dan apakah harga dasar tanpa diskon tadi lebih dari Rp 100.000
-            total:= total * 0.90; //jika iya, nah dapat diskon 10%
-        end
-        else if (prem = 'False') or (prem = 'false') then //bukan anggota premium?
-        begin 
-            if (total1 > 100000) then //apakah harga dasar lebih dari Rp 100.000
-            total:= total1 * 0.90; //kalau iya, nah dapat diskon 10%, walaupun tidak premium
-        end
-    end //berakhir bagian jika km lebih dari 10
-
-    else if (km >= 5) and (km <= 10) then //jika km bukan lebih dari 10 maka, apakah km antara 5 dan 10?
-    begin //mulai jika iya
-     total1:= km*4000 + 20000; //menghitung dasar harga tanpa diskon
-        if (prem = 'True') or (prem = 'true') then //apakah anggota premium
-        begin //mulai jika anggota premium
-        total:= total1 * 0.95; //kalau anggota premium dapat diskon 5%
-            if (total1 > 100000) then //dan apakah harga dasar tanpa diskon tadi lebih dari Rp 100.000
-            total:= total * 0.90; //jika iya, nah dapat diskon 10%
-        end
-        else if (prem = 'False') or (prem = 'false') then //bukan anggota premium?
-        begin 
-            if (total1 > 100000) then //apakah harga dasar lebih dari Rp 100.000
-            total:= total1 * 0.90; //kalau iya, nah dapat diskon 10%, walaupun tidak premium
-        end
-    end //berakhir bagian jika km antara 5 dan 10
-
-    else if (km < 5) then //jika km bukan antara 5 dan 10 maka, apakah km kurang dari 5?
-    begin //mulai jika iya
-     total1:= km*5000 + 20000; //menghitung dasar harga tanpa diskon
-        if (prem = 'True') or (prem = 'true') then //apakah anggota premium
-        begin //mulai jika anggota premium
-        total:= total1 * 0.95; //kalau anggota premium dapat diskon 5%
-            if (total1 > 100000) then //dan apakah harga dasar tanpa diskon tadi lebih dari Rp 100.000
-            total:= total * 0.90; //jika iya, nah dapat diskon 10%
-        end
-        else if (prem = 'False') or (prem = 'false') then //bukan anggota premium?
-        begin 
-            if (total1 > 100000) then //apakah harga dasar lebih dari Rp 100.000
-            total:= total1 * 0.90; //kalau iya, nah dapat diskon 10%, walaupun tidak premium
-        end
-    end; //berakhir bagian jika km kurang dari 5
-    Write('Total Akhir : ',total:0:2); //hasil total yang harus dibayar
+    clrscr; //bersihkan hal gak penting
+    Write('Jarak (KM) : '); readln(km); //Input jarak dalam (KM)
+    Write('Anggota Premium : '); readln(prem); //Input Apakah anggota premium 
+begin //memulai program if
+    if (km < 5) then //jika km kurang dari 5
+        total:= km*5000 + 20000 //maka akan menghitung sesuai ketentuan
+    else if (km >= 5) and (km <= 10) then //jika km diantara 5 dan 10 
+        total:= km*4000 + 20000 //maka akan menghitung sesuai ketentuan
+    else //jika tidak juga berarti km pasti lebih dari 10
+        total:= km*3000 + 20000; //maka akan di hitung sesuai ketentuannya
+    total1:= total; //menginisialisasi total sama dengan total1
+    if (total1 > 100000) and (prem = 'True') or (prem = 'True') then //jika harga total1 itu lebih dari 100000 dan dia premium
+        total1:= total1*0.85 //maka akan dapat diskon sebesar 15%
+    else if (prem = 'True') or (prem = 'true') then //jika hanya anggota premium saja
+        total1:= total1*0.95 //maka akan dapat diskon 5%
+    else if (total1 > 100000) then //jika cuma harga total1 yang leibh dari 100000 dan tidak premium
+        total1:= total1*0.90 //maka akan mendapat diskon 10%
+    else //jika tidak juga, ya gak ada diskon
+        total1:=total; //menginialisasi total sama dengan total1
+end; //selesai program if
+    Writeln('Total Akhir : ',total1:0:2); //output(hasil)
 end. //selesai
